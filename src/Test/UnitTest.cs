90d48cef2423c8ef9d74172c4a077efe6106d579
Jiadong Huang (Wicresoft) <v-jiadh@microsoft.com>
2016-03-02 14:11:26 +0800

    Add math parser project file

---
+++ src/Test/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathExpressionParser;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var str = "-1*2+5-6/2*3";
+            Expr expr = new Expr(str);
+            var result = expr.Calculate();
+            Assert.IsTrue(result == -6);
+        }
+    }
+}
